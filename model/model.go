dd66e430367bab799770cf9f6e8bd1b845400ec1
dmitry <dzmitry.zenin@gmail.com>
2022-09-10 22:36:01 +0200

    mino

---
+++ model/model.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"regexp"
+	"time"
+)
+
+// uuidMatcher can be tested here: https://regex101.com/r/jh0MuE/latest
+var uuidMatcher = regexp.MustCompile(`^[a-fA-F0-9]{8}(?:-?[a-fA-F0-9]{4}){3}-?[a-fA-F0-9]{12}$`)
+
+type UUID string
+
+func (u UUID) IsValid() bool {
+	return uuidMatcher.MatchString(string(u))
+}
+
+func (u UUID) String() string {
+	return string(u)
+}
+
+type Newsletter struct {
+	ID      string
+	Title   string
+	Body    string
+	Created time.Time
+	Updated time.Time
+}
